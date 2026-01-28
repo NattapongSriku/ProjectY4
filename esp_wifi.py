1d69b62141eb66d0a7598bf4b5e4a97a37a6422c
NattapongSriku <maxkungza007@gmail.com>
2026-01-28 13:27:02 +0700

    Add files via upload

---
+++ esp_wifi.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import requests
+
+ESP32_IP = "192.168.1.92"   # IP ของ ESP32
+
+st.title("AC Control")
+
+if st.button("AC ON"):
+    requests.get(f"http://http://192.168.1.92/on")
+    st.success("AC ON sent")
+
+if st.button("AC OFF"):
+    requests.get(f"http://192.168.1.92/off")
+    st.success("AC OFF sent")
